16f369ce0702be9e11f0a0b0b5e9eee01300dc50
Ryan Allen <rallen3882@gmail.com>
2020-02-29 17:18:26 -0500

    Init commit

---
+++ src/test/java/ninja/ryanallen/dbapp/DbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ninja.ryanallen.dbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
